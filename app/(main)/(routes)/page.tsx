e167393b6c4182fcb825d6d8815ff913ff5f72b6
Sayakdutt <sayakofficial01@gmail.com>
2023-11-26 15:53:21 +0530

    first commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "@/components/providers/mode-toggle"
+import { UserButton } from "@clerk/nextjs"
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton 
+        afterSignOutUrl="/"
+      />
+      <ModeToggle />
+    </div>
+  )
+}
