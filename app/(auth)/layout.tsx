b7504f7bbd48e66c376481518bcfe8b7b006b41c
Sayakdutt <sayakofficial01@gmail.com>
2023-11-30 21:49:40 +0530

    Auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full bg-indigo-500 flex items-center justify-center">
+    <div className="h-full bg-white flex items-center justify-center">
       {children}
     </div>
   );
